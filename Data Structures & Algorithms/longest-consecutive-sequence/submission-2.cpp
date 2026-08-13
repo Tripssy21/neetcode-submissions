ead02ca9c70157b2760eb341f8bfabe14f9c7096
Aditya Gupta <80877310+Tripssy21@users.noreply.github.com>
2026-08-13 15:02:25 +0530

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, pair<int, int>> hashMap;
+        for(int num : nums){
+            if(!hashMap.contains(num - 1) && !hashMap.contains(num + 1))
+                hashMap[num] = {num, num};
+            else if(!hashMap.contains(num - 1) && hashMap.contains(num + 1)){
+                hashMap[num] = {num, hashMap.contains(num) ? max(hashMap[num].second, hashMap[num + 1].second) : hashMap[num + 1].second};
+                hashMap[hashMap[num + 1].second] = {num, hashMap[num].second};
+            }
+            else if (hashMap.contains(num - 1) && !hashMap.contains(num + 1)){
+                hashMap[num] = {hashMap.contains(num) ? min(hashMap[num - 1].first, hashMap[num].first) : hashMap[num - 1].first, num};
+                hashMap[hashMap[num - 1].first] = {hashMap[num].first, num};
+            }
+            else{
+                hashMap[num] = {hashMap.contains(num) ? min(hashMap[num - 1].first, hashMap[num].first) : hashMap[num - 1].first, hashMap.contains(num) ? max(hashMap[num].second, hashMap[num + 1].second) : hashMap[num + 1].second}; 
+                hashMap[hashMap[num - 1].first] = {hashMap[num].first, hashMap[num].second};
+                hashMap[hashMap[num + 1].second] = {hashMap[num].first, hashMap[num].second};
+            } 
+        }
+        int result = 0;
+        for(auto& [a, b] : hashMap)
+            result = max(result, b.second - b.first + 1);
+        return result;
+    }
+};
