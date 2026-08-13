5c53f7d18987db8e264cead51fc844fa56791f8d
Aditya Gupta <80877310+Tripssy21@users.noreply.github.com>
2026-08-13 21:17:59 +0530

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string new_s = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] > 64 & s[i] < 91)
+                new_s += (char)(s[i] + 32);
+            else if((s[i] > 47 && s[i] < 57) ||(s[i] > 96 && s[i] < 123))
+                new_s += s[i];
+            
+        // cout << new_s << endl;
+        }
+        for(int i = 0; i < new_s.length(); i++){
+            if(tolower(new_s[i]) != tolower(new_s[new_s.length() - 1 - i]))
+                return false;
+        }
+        return true;
+    }
+};
