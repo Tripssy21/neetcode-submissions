0846e49b15de782f484870cfdb186b0a5c13260d
Aditya Gupta <80877310+Tripssy21@users.noreply.github.com>
2026-08-13 17:17:12 +0530

    Add: is-palindrome - submission-8

---
+++ Data Structures & Algorithms/is-palindrome/submission-8.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j){
+            if(!((tolower(s[i]) >= 'a' && tolower(s[i]) <= 'z') || (s[i] >= '0' && s[i] <= '9'))){
+                i++;
+                continue;
+            }
+            if(!((tolower(s[j]) >= 'a' && tolower(s[j]) <= 'z') || (s[j] >= '0' && s[j] <= '9'))){
+                j--;
+                continue;
+            }
+            if(tolower(s[i]) != tolower(s[j]))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
